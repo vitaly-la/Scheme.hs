be7c229d9678c365ac2e12ca94e3c531783946c8
vitaly-la <148568747+vitaly-la@users.noreply.github.com>
2024-12-22 13:50:41 +0000

    some list functions

--- Main.hs
+++ Main.hs
@@ -1,11 +1,10 @@
 {-# OPTIONS -Wno-missing-methods #-}
 
 import Control.Monad.State.Lazy
-import Data.Map.Lazy ((!))
 import qualified Data.Map.Lazy as M
 import Text.Read (readMaybe)
 
-data Expression = Number Integer | Word String | List [Expression] | End
+data Expression = Number Integer | Word String | List [Expression] | End deriving (Eq)
 
 instance Show Expression where
     show (Number number) = show number
@@ -18,13 +17,9 @@ instance Num Expression where
     Number x * Number y = Number $ x * y
     fromInteger x = Number x
 
-instance Eq Expression where
-    Number x == Number y = x == y
-    Word x == Word y = x == y
-    List xs == List ys = xs == ys
-
 unbracket [] = []
 unbracket ('(':xt) = "(" : unbracket xt
+unbracket ('\'':xt) = "'" : unbracket xt
 unbracket (x:xt) = case unbracket xt of
     [] -> [[x]]
     ys@(")":_) -> [x] : ys
@@ -39,23 +34,26 @@ collectSubexprs = do
             return (expr : rest)
 
 readExpr = do
-    (tokens, _, _) <- get
+    (tokens, var, fns) <- get
     case tokens of
         [] -> return End
-        (token:_) -> do
-            modify $ \(tokens, var, fns) -> (tail tokens, var, fns)
+        (token:rest) -> do
+            put (rest, var, fns)
             case token of
                 "(" -> do
                     subexprs <- collectSubexprs
                     return $ List subexprs
                 ")" -> return End
+                "'" -> do
+                    expr <- readExpr
+                    return $ List [Word "quote", expr]
                 _ -> case readMaybe token of
                     Just number -> return $ Number number
                     _ -> return $ Word token
 
 getVariable name = do
     (tokens, var, fns) <- get
-    return $ var ! name
+    return $ var M.! name
 
 setVariable name value = modify $ \(tokens, var, fns) -> (tokens, M.insert name value var, fns)
 
@@ -64,21 +62,23 @@ setVariables ((Word name, value):rest) = do
     setVariable name value
     setVariables rest
 
+setFunction name fargs body = modify $ \(tokens, var, fns) -> (tokens, var, M.insert name (fargs, body) fns)
+
 eval expr = case expr of
     Number _ -> return expr
     Word name -> getVariable name
     List [] -> return expr
     List (Word fn:args) -> case fn of
-        "quote" -> return $ head args
+        "quote" -> return $ args !! 0
         "define" -> case args of
             [Word name, value] -> do
                 setVariable name value
                 return $ Word name
             [List (Word name:fargs), body] -> do
-                modify $ \(tokens, var, fns) -> (tokens, var, M.insert name (fargs, body) fns)
+                setFunction name fargs body
                 return $ Word name
         "if" -> do
-            value <- eval $ head args
+            value <- eval $ args !! 0
             case value of
                 0 -> eval $ args !! 2
                 _ -> eval $ args !! 1
@@ -92,9 +92,12 @@ eval expr = case expr of
                 "*" -> return $ product args
                 "=" -> return $ if all (head args ==) $ tail args then 1 else 0
                 "cons" -> let [x, List xt] = args in return $ List (x : xt)
+                "car" -> let List (x:xt) = args !! 0 in return x
+                "cdr" -> let List (x:xt) = args !! 0 in return $ List xt
+                "length" -> let List xs = args !! 0 in return . Number . toInteger $ length xs
                 _ -> do
                     (_, _, fns) <- get
-                    let (fargs, body) = fns ! fn
+                    let (fargs, body) = fns M.! fn
                     setVariables $ zip fargs args
                     eval body
 
